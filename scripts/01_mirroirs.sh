f1c7173cb8e6eda48623cc4f4aeeac8cab7e2509
arllinux <jeanpierre.antinoux@free.fr>
2017-12-31 22:03:32 +0100

    ...

--- scripts/01_mirroirs.sh
+++ scripts/01_mirroirs.sh
@@ -6,17 +6,17 @@
 
 # Faire une copie du ficher mirrorlist.
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-echo ":: Copie du fichier mirrorlist éffectuée ::"
 
 # Décommenter TOUS les miroirs dans mirrorlist.backup.
 # Pour se faire, sed s'avère très utile.
 sed -s 's/^#Server/Server/' /etc/pacman.d/mirrorlist.backup
+echo ":: Copie du fichier mirrorlist éffectuée ::"
 echo ":: Fichier mirrorlist.backup décommenté ::"
 
 # Rankmirrors trouve les 10 meilleurs miroirs
 # Il écrit le résultat directement dans /etc/pacman.d/mirrorlist
-echo ":: Si la commande dure plus de 15 secondes ::"
-echo ":: Arrêtez-la avec Ctrl + c .............. ::"
+echo ":: Le test peut durer jusqu'à 5 minutes ::"
+echo ":: Soyez patient ! Allez vous faire un café :-) ::"
 rankmirrors -n 10 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
 
 echo ":: Le fichier /etc/pacman.d/mirrorlist est à jour ! ::"
